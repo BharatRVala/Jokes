727c7ef43be90db8230d9c65662d4a3f83983991
Bharat vala <bharatkumarramashibhai2003@gmail.com>
2025-01-22 13:50:26 +0530

    Resolved merge conflict in page.js

--- src/app/jokes/page.js
+++ src/app/jokes/page.js
@@ -4,11 +4,7 @@ export default async function JokesPage() {
   let jokes = [];
 
   try {
-<<<<<<< HEAD
     const response = await fetch('/api/alljokes', {
-=======
-    const response = await fetch('api/alljokes', {
->>>>>>> 0eece9bbc5f2c7e948d1c76de5404f8ef954b80b
       cache: 'no-store',
     });
     const data = await response.json();
